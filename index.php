4a368419b9e17d2f69b1be35dfc88214ae2ece75
leo <thomasleo723@gmail.com>
2026-03-30 11:56:37 +0200

    mise en place php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="efr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>acceuil</title>
+</head>
+
+<body>
+    <?php
+    include("header.php")
+        ?>
+    <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
+        <h1>Bienvenue sur le site officiel du lycée Kernanec de Marcq en Baroeul !</h1>
+        <h5>Vous souhaitez devenir élève ou parent d’élève du lycée Kernanec... Ce site a été conçu pour vous !</h5>
+
+        <p>Vous y trouverez un maximum d’informations pratiques, notamment dans la rubrique "Présentation - Devenir
+            élève au
+            lycée Kernanec".</p>
+
+        <h4>N’hésitez pas à nous contacter pour en savoir davantage !</h4>
+
+        <p>Michel FAUQUETTE, Proviseur</p>
+
+        <p>Rentrée des élèves
+            2ndes le lundi 3 septembre de 9h à 12h
+            1ères et Terminales le lundi 3 septembre de 14h à 17h</p>
+        </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
