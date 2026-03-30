018e5695d8c2d1b20415d3ac408e8ebdd4ca49c3
leo <thomasleo723@gmail.com>
2026-03-30 21:57:19 +0200

    fin jour 1

---
+++ seconde.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devenir élève</title>
+
+</head>
+
+<body>
+    <?php
+    include("header.html")
+        ?>
+    <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
+        <h3><i class="fa-solid fa-message"></i>Les enseignements d’exploration en
+            Seconde</h3>
+
+        <p>En seconde, en complément des enseignements généraux communs tels que le français ou les mathématiques, les
+            élèves
+            suivent deux enseignements d’exploration de 1 h 30 (ou 3h) par semaine chacun. Les objectifs de ces
+            enseignements sont :
+
+        <ol>
+            <ul>
+                <li>faire découvrir des champs disciplinaires de connaissances et les méthodes associées</li>
+                <li>informer sur les cursus possibles au cycle terminal et dans le supérieur : IUT, classes
+                    préparatoires,
+                    université,
+                    etc.</li>
+                <li>identifier les activités professionnelles auxquelles ces cursus peuvent conduire</li>
+            </ul>
+        </ol>
+
+
+        Chacun peut ainsi expérimenter de nouvelles disciplines, afin de faciliter ensuite ses choix d’orientation.</p>
+
+        <p><strong>Les enseignements d’exploration suivis ne conditionnent en rien l’orientation en première.</strong>
+        </p>
+
+        <p><strong>Deux enseignements au choix</strong></p>
+
+        <p>Au lycée Kernanec, tous les élèves suivent l’enseignement d’exploration "<strong>sciences économiques et
+                sociales</strong>".
+            Il
+            s’agit de
+            découvrir les savoirs et méthodes spécifiques à la science économique et à la sociologie, à partir de
+            quelques
+            grandes
+            problématiques contemporaines.</p>
+
+        <p>Le deuxième enseignement d’exploration est à choisir parmi :</p>
+
+        Latin
+        • explorer trois sujets d’étude : l’homme romain, le monde romain, les figures héroïques et mythologiques
+        • pratiquer la lecture des textes, notamment un extrait significatif d’une œuvre intégrale
+
+        Littérature et société
+        • connaître les enjeux, la variété et l’intérêt d’une formation littéraire et humaniste
+        • percevoir les interactions entre la littérature, l’histoire et la société
+        • explorer la diversité des cursus et des activités professionnelles liées aux études littéraires
+
+        Méthodes et pratiques scientifiques
+        • se familiariser avec les démarches scientifiques autour de projets interdisciplinaires
+        • comprendre l’apport et la place des sciences dans les grandes questions de société
+        • découvrir certains métiers et formations scientifiques
+
+        Sciences et laboratoire
+        • découvrir et pratiquer des activités scientifiques en laboratoire
+        • apprendre à connaître et utiliser des méthodologies et des outils propres aux différentes disciplines
+        scientifiques
+        concernées
+
+        Informatique et Création Numérique
+        L’ICN (informatique et création numérique) est un enseignement d’exploration proposé en classe de seconde au
+        lycée
+        Kernanec à compter de la rentrée 2018. En 1h30 chaque semaine et sans prérequis, cet enseignement propose une
+        initiation
+        à la programmation : conception d’un site web ou création d’un jeu simple. L’apprentissage se fait par la
+        pratique, les
+        séances se déroulent donc uniquement en salle informatique avec des petits exercices pour comprendre puis des
+        mini-projets pour mettre en application ses connaissances.
+        L’ICN pourra éventuellement être poursuivi en première et terminale en tant qu’option menant à une épreuve
+        facultative
+        au baccalauréat.
+
+        (Sources : Ministère de l’Education Nationale) et eduscol
+
+        Au moment de l’inscription, l’élève formule des vœux pour le deuxième enseignement d’exploration. La répartition
+        dans
+        les groupes tient compte de ces vœux et des places disponibles.
