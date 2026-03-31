9ab80743bc3d90de3c1e805401be7225112e9818
leo <thomasleo723@gmail.com>
2026-03-31 09:50:12 +0200

    header + terminale + anglais

---
+++ anglaisEuro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devenir élève</title>
+
+</head>
+
+<body>
+    <?php
+    include("header.html")
+        ?>
+    <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
+        <h3><i class="fa-solid fa-message"></i>Les enseignements du cycle terminale</h3>
+        <P><strong>Particularités des enseignements</strong></P>
+
+        <p>L’ enseignement dispensé dans cette section repose sur les programmes en vigueur. Il est cependant aménagé :
+            au
+            lycée Kernanec, l’histoire géographie est enseignée en partie en anglais pour les élèves de la section, de
+            la
+            2nde à la Terminale. C’est ce que l’on appelle la discipline non linguistique (D.N.L.).
+            Le choix de cette option n’a absolument aucune influence sur les contenus des autres disciplines. En
+            d’autres
+            termes, le choix de l’option Anglais européen ne bloque pas le passage dans une première scientifique ou
+            n’engage pas les élèves à suivre une première à vocation littéraire.
+            Les élèves de section européenne anglais sont répartis dans plusieurs classes, ils ne sont pas tous réunis
+            dans
+            la même classe.</p>
+
+        <p>Volumes horaires :</p>
+
+        <P><strong>Particularités du baccalauréat</strong></P>
+
+        <p>L’indication "section européenne anglais" est inscrite sur le diplôme du baccalauréat.<br>
+            Cette indication dépend des résultats obtenus par le candidat :</p>
+    </main>
+</body>
+
+</html>
---
+++ terminale.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devenir élève</title>
+
+</head>
+
+<body>
+    <?php
+    include("header.html")
+        ?>
+    <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
+        <h3><i class="fa-solid fa-message"></i>Les enseignements du cycle terminale</h3>
+        <ul>
+            <li>
+                <strong>Premières</strong>
+                <ul>
+                    <li>
+                        <strong>Série L</strong>
+                        <ul>
+                            <li>Un enseignement spécifique obligatoire au choix parmi :
+                                <ul>
+                                    <li>C.A.V.</li>
+                                    <li>Latin</li>
+                                    <li>Anglais approfondi</li>
+                                    <li>Maths</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <strong>Séries L, ES et S</strong>
+                        <ul>
+                            <li>Deux enseignements facultatifs au plus parmi :
+                                <ul>
+                                    <li>Latin</li>
+                                    <li>Cinéma Audio Visuel</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <strong>Terminales</strong>
+                <ul>
+                    <li>
+                        <strong>Série ES</strong>
+                        <ul>
+                            <li>Un enseignement obligatoire de spécialité au choix parmi :
+                                <ul>
+                                    <li>Mathématiques</li>
+                                    <li>Sciences Sociales et Politiques</li>
+                                    <li>Economie approfondie</li>
+                                </ul>
+                            </li>
+                            <li>Deux enseignements facultatifs au plus parmi :
+                                <ul>
+                                    <li>Latin</li>
+                                    <li>C.A.V.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <strong>Série L</strong>
+                        <ul>
+                            <li>Un enseignement obligatoire de spécialité au choix parmi :
+                                <ul>
+                                    <li>C.A.V.</li>
+                                    <li>Mathématiques</li>
+                                    <li>Latin</li>
+                                    <li>Anglais approfondi</li>
+                                    <li>Droits et Grands Enjeux du Monde Contemporain</li>
+                                </ul>
+                            </li>
+                            <li>Deux enseignements facultatifs au plus parmi :
+                                <ul>
+                                    <li>Latin</li>
+                                    <li>C.A.V.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <strong>Série S</strong>
+                        <ul>
+                            <li>Un enseignement obligatoire de spécialité au choix parmi :
+                                <ul>
+                                    <li>Physique Chimie</li>
+                                    <li>SVT</li>
+                                    <li>Mathématiques</li>
+                                </ul>
+                            </li>
+                            <li>Deux enseignements facultatifs au plus parmi :
+                                <ul>
+                                    <li>Latin</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </main>
+</body>
+
+</html>
