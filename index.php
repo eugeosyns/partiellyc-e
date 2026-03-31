741a3e274af7711e03273399fbd3c6e08843e5bf
leo <thomasleo723@gmail.com>
2026-03-31 21:20:04 +0200

    pictograms

--- index.php
+++ index.php
@@ -11,21 +11,26 @@
     <?php
     include("header.html")
         ?>
-    <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
-        <h1>Bienvenue sur le site officiel du lycée Kernanec de Marcq en Baroeul !</h1>
-        <h5>Vous souhaitez devenir élève ou parent d’élève du lycée Kernanec... Ce site a été conçu pour vous !</h5>
+    <main class="d-flex flex-column mx-auto mt-5 gap-3">
+        <div class="d-flex flex-wrap justify-content-around gap">
+            <div>
+                <h1 class="titre">Bâtir votre réussite au Lycée Kernanec</h1>
+                <p>site officiel de kernanec</p>
+            </div>
+            <img src="img/batimentface.png" alt="batiment vue de face" class="rond imgacceuil">
 
-        <p>Vous y trouverez un maximum d’informations pratiques, notamment dans la rubrique "Présentation - Devenir
-            élève au
-            lycée Kernanec".</p>
+        </div>
 
-        <h4>N’hésitez pas à nous contacter pour en savoir davantage !</h4>
-
-        <p>Michel FAUQUETTE, Proviseur</p>
-
-        <p>Rentrée des élèves
-            2ndes le lundi 3 septembre de 9h à 12h
-            1ères et Terminales le lundi 3 septembre de 14h à 17h</p>
+        <div class="d-flex flex-wrap">
+            <div class="card rounded-5" style="width: 18rem;">
+                <img src="img/inscription.svg" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
     </main>
 
