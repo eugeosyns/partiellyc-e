c182dc8f5292fe93fdda48a41dd4da448dcd148e
leo <thomasleo723@gmail.com>
2026-04-01 13:55:02 +0200

    bouton contact header

---
+++ motproviseur.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mot du proviseur</title>
+</head>
+
+<body>
+    <?php
+    include("header.html")
+        ?>
+    <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
+        <p>
+        <h3><i class="fa-solid fa-message"></i></i>Mot du proviseur</h3>
+
+        <p>Le lycée Yves Kernanec scolarise environ 840 élèves de la seconde à la terminale (Bacs L, ES et S).</p>
+        C’est un lycée à taille humaine dans un environnement agréable qui offre de très bonnes conditions de travail et
+        de
+        réussite.</p>
+
+        <p>Les équipes enseignantes sont attachées à la réussite de tous les élèves - tant pour l’obtention du bac que
+            pour
+            une
+            orientation choisie - et s’impliquent pour parvenir à ce but.</p>
+
+        <p>Un suivi attentif et rigoureux de la Vie Scolaire renforce notre action, s’harmonise avec la Pédagogie pour
+            une
+            individualisation du parcours. L’ensemble des personnels reste à l’écoute des élèves et des familles tout au
+            long de
+            l’année. N’hésitez pas à les rencontrer !</p>
+
+        <p>Il est bien évident que la réussite est indissociable de la volonté et d’un travail sérieux et régulier de la
+            part
+            de chaque jeune.</p>
+
+        <p>"La vraie générosité envers l’avenir est de tout donner au présent". (A. Camus)</p>
+
+        <p>Ceci résume bien notre objectif qui doit être commun avec celui des élèves et des familles : l’avenir se
+            prépare
+            dès
+            aujourd’hui, en donnant le meilleur de soi-même.</p>
+
+        <p>Je souhaite à tous nos élèves une bonne rentrée et une excellente année 2017-2018. J’espère qu’elle leur
+            apportera à
+            la fois épanouissement personnel et réussite scolaire.</p>
+
+        <p>Bienvenue au lycée Kernanec !</p>
+
+        <p>Michel Fauquette</p>
+
+        <p>PS : Un grand merci à Cédric F., ancien élève du lycée et ingénieur Telecom Lille, pour son réel
+            investissement
+            pour
+            la création de ce site.</p>
+    </main>
+    <?php
+    include("footer.php")
+        ?>
+</body>
+
+</html>
