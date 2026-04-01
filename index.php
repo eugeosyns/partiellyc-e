421cf6b452629f283ee495b9af5a2cfd5f9ef3ce
leo <thomasleo723@gmail.com>
2026-04-01 09:39:02 +0200

    pictograms

--- index.php
+++ index.php
@@ -20,15 +20,53 @@
             <img src="img/batimentface.png" alt="batiment vue de face" class="rond imgacceuil">
 
         </div>
-
         <div class="d-flex flex-wrap">
-            <div class="card rounded-5" style="width: 18rem;">
-                <img src="img/inscription.svg" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="d-flex flex-wrap">
+                <div class="card rounded-5" style="width: 18rem;">
+                    <img src="img/fichier.svg" alt="..." class="imgcard">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-wrap">
+                <div class="card rounded-5" style="width: 18rem;">
+                    <img src="img/calendar.svg" alt="..." class="imgcard">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-wrap">
+                <div class="card rounded-5" style="width: 18rem;">
+                    <img src="img/parents.svg" alt="..." class="imgcard">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-wrap">
+                <div class="card rounded-5" style="width: 18rem;">
+                    <img src="img/postbac.svg" alt="..." class="imgcard">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
         </div>
