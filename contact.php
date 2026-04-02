991b75e9ae70040b5022aa161468e5cdafc3d1de
leo <thomasleo723@gmail.com>
2026-04-02 16:24:05 +0200

    logo dans l'onglet

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le conseil</title>
+    <title>Contact</title>
 
 </head>
 
@@ -16,18 +16,18 @@
         <h3><i class="fa-solid fa-message"></i>Nous contacter</h3>
         <form action="" class="d-flex flex-column w-50 gap-1 mx-auto">
             <label for="name">name</label>
-            <input type="text" name="name">
+            <input type="text" name="name" class="form-control">
             <label for="lastname">lastname</label>
-            <input type="text" name="lastname">
+            <input type="text" name="lastname" class="form-control">
             <label for="tel">téléphone</label>
-            <input type="tel" name="tel">
+            <input type="tel" name="tel" class="form-control">
             <label for="mail">mail</label>
-            <input type="mail" name="mail">
+            <input type="mail" name="mail" class="form-control">
             <label for="sujet">sujet</label>
-            <input type="text" name="sujet">
+            <input type="text" name="sujet" class="form-control">
             <label for="message">message</label>
-            <input type="textearea" name="message">
-            <button type="submit" class="w-50 mx-auto">envoyer</button>
+            <input type="textearea" name="message" class="form-control">
+            <button type="submit" class="w-50 mx-auto" class="form-control">envoyer</button>
         </form>
     </main>
     <?php
