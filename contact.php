02b981c3b936b4958f937324caaeb4537399c0e4
leo <thomasleo723@gmail.com>
2026-04-02 16:17:09 +0200

    css contact

--- contact.php
+++ contact.php
@@ -14,12 +14,12 @@
         ?>
     <main class="d-flex flex-column w-75 mx-auto mt-2 gap-3">
         <h3><i class="fa-solid fa-message"></i>Nous contacter</h3>
-        <form action="">
+        <form action="" class="d-flex flex-column w-50 gap-1 mx-auto">
             <label for="name">name</label>
             <input type="text" name="name">
             <label for="lastname">lastname</label>
             <input type="text" name="lastname">
-            <label for="tel"></label>
+            <label for="tel">téléphone</label>
             <input type="tel" name="tel">
             <label for="mail">mail</label>
             <input type="mail" name="mail">
@@ -27,7 +27,7 @@
             <input type="text" name="sujet">
             <label for="message">message</label>
             <input type="textearea" name="message">
-            <button type="submit"></button>
+            <button type="submit" class="w-50 mx-auto">envoyer</button>
         </form>
     </main>
     <?php
